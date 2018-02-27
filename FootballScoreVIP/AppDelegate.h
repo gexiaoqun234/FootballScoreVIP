a2ec357f215b51b158e4a0ed24425a17672da5dc
tianwei <tianwei223344@163.com>
2018-02-27 16:57:29 +0800

    Initial Commit

---
+++ FootballScoreVIP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FootballScoreVIP
+//
+//  Created by 田伟 on 2018/2/27.
+//  Copyright © 2018年 MG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FootballScoreVIP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FootballScoreVIP
+//
+//  Created by 田伟 on 2018/2/27.
+//  Copyright © 2018年 MG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
